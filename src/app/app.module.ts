6e842a17ab98a5512fde3b3fed48634dd0112d9a
Nico Koenig <npunktkoenig@gmail.com>
2018-11-18 21:59:52 +0100

    feature: display babyevents in timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import de from "@angular/common/locales/de";
+import { registerLocaleData } from "@angular/common";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { TimelineComponent } from "./timeline/timeline.component";
+import { SettingsComponent } from "./settings/settings.component";
 
 import {
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
   MatSidenavModule,
-  MatListModule
+  MatListModule,
+  MatCardModule
 } from "@angular/material";
 
-import { TimelineComponent } from "./timeline/timeline.component";
-import { SettingsComponent } from "./settings/settings.component";
+registerLocaleData(de);
 
 @NgModule({
   declarations: [AppComponent, TimelineComponent, SettingsComponent],
@@ -26,9 +31,10 @@ import { SettingsComponent } from "./settings/settings.component";
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "de-de" }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BabywatchService } from "../babywatch.service";
 
 @Component({
-  selector: 'bw-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  selector: "bw-timeline",
+  templateUrl: "./timeline.component.html",
+  styleUrls: ["./timeline.component.scss"]
 })
 export class TimelineComponent implements OnInit {
+  constructor(private babyService: BabywatchService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getTimeline() {
+    return this.babyService.getTimeline();
   }
-
+  getBabyName() {
+    return this.babyService.babyName || "Das Baby";
+  }
 }
